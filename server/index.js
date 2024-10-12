ba2d7a54ad097649289cdf4c4673d19f66e49946
Eumelle Jeo Mamacos <jeomamacosschool@gmail.com>
2024-10-12 09:45:38 +0800

    Todos Routes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express"
+import router from "./routes/todos.js"
 
 const port = 3000;
 const app = express();
@@ -6,6 +7,8 @@ const app = express();
 app.get('/',(req,res) =>{
     res.send('Hello Todo App!!!');
 });
+
+app.use("/api", router);
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
